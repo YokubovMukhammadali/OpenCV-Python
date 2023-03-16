4206cda7db484cb7450e36d9f425f8fb5305b1c8
Yokubov Mukhammadali <119654152+YokubovMukhammadali@users.noreply.github.com>
2023-03-16 03:47:06 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+
+import cv2
+
+img_basic = cv2.imread('ios.jpg',cv2.IMREAD_COLOR)
+
+cv2.imshow('Image Basic',img_basic)
+
+cv2.waitKey(0)
+
+cv2.imwrite('result1.png', img_basic)
+
+cv2.destroyAllWindows()
+
+img_gray = cv2.cvtColor(img_basic,cv2.COLOR_BGR2GRAY)
+
+cv2.imshow('Image Gray',img_gray)
+
+cv2.waitKey(0)
+
+cv2.imwrite('result1.png',img_gray)
